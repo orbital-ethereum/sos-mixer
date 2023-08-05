d24823c4fe0abf2701979dee32cce45de8597ec1
summerofshibarium <141514194+summerofshibarium@users.noreply.github.com>
2023-08-05 13:40:32 -0500

    Add files via upload

---
+++ precache-manifest.9731dc17411a80acfda3f08a19e9f9cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfa3e68e41d88544c939ec94508e12e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "928851d055e3721b12d1",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c478eff81cef86d13588",
+    "url": "/static/js/2.5e77b4bd.chunk.js"
+  },
+  {
+    "revision": "928851d055e3721b12d1",
+    "url": "/static/js/main.6e934b43.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
